4c089c31af8b7fabc7243ddd71ee8e4bb3b8d209
hassan-15 <sonysoliman5@gmail.com>
2022-04-01 18:08:20 +0200

    ver 2

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,23 +35,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         backgroundColor: Colors.white54,
         appBar: AppBar(
@@ -74,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
           ),
           actions: [
-            VerticalDivider(),
+            //VerticalDivider(),
             Container(
               //decoration: BoxDecoration(
               //color: Colors.grey.withOpacity(0.3), shape: BoxShape.circle),
@@ -102,39 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
               SizedBox(
                 height: 10,
               ),
-              // ...List.generate(
-              //     20,
-              //     (index) =>
-              //     ListTile(
-              //           leading: CircleAvatar(
-              //             backgroundImage: AssetImage('assets/images/Hassan.jpg'),
-              //             radius: 30,
-              //           ),
-              //           title: Text(
-              //             'Hassan Ali ',
-              //             style: TextStyle(
-              //               color: Colors.black,
-              //               fontWeight: FontWeight.bold,
-              //               fontSize: 14,
-              //             ),
-              //           ),
-              //           subtitle: Text(
-              //             'Developer Career is the best! ',
-              //             style: TextStyle(
-              //               color: Colors.grey,
-              //               fontSize: 14,
-              //             ),
-              //           ),
-              //           trailing: Text(
-              //             '12:00:PM',
-              //             style: TextStyle(
-              //               color: Colors.black,
-              //               fontSize: 14,
-              //             ),
-              //           ),
-              //         )
-              // )
-
               Container(
                 height: MediaQuery.of(context).size.height ,
                 width: double.infinity,
@@ -160,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                         ),
                         subtitle: Text(
-                          'Developer Career is the best! ',
+                          'Moaaz is the best! ',
                           style: TextStyle(
                             color: Colors.grey,
                             fontSize: 14,
